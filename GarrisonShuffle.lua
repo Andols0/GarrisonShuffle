2e55eba604d786017f3b91c721eb03720ec402e1
Andols <github@andols.se>
2022-10-25 23:17:08 +0200

    Update for 10.0

--- GarrisonShuffle.lua
+++ GarrisonShuffle.lua
@@ -1,4 +1,4 @@
-local Shown, Nametable
+local Shown, Nametable, SortList
 
 ------------Random Button
 local RandomButton= CreateFrame("Button", "$parentRandomButton", GossipFrame, "UIPanelButtonTemplate")
@@ -7,103 +7,57 @@ RandomButton:SetSize(78,22)
 RandomButton:SetText("Shuffle")
 RandomButton:Hide()
 RandomButton:SetScript("OnClick", function()
-	local strings=C_GossipInfo.GetOptions()
-	local number=random(1,C_GossipInfo.GetNumOptions())
-	C_GossipInfo.SelectOption(number)
-		if string.find(strings[number].name,"Default") then
+	local Options = C_GossipInfo.GetOptions()
+	local number = random(1,#Options)
+	C_GossipInfo.SelectOption(Options[number].gossipOptionID)
+	if string.find(Options[number].name,"Default") then
 		print("Now playing Default Garrison Music")
 	else
-		print("Now playing "..string.sub(strings[number].name,7,-2))
+		print("Now playing "..string.sub(Options[number].name,7,-2))
 	end
 	end)
 
 -----------CheckButton
-local SortOption=CreateFrame("CheckButton", "$parentSortButton", GossipFrame, "OptionsSmallCheckButtonTemplate")
-SortOption:SetPoint("BOTTOMLEFT",GossipFrame, "BOTTOMLEFT",80,1)
+local SortOption = CreateFrame("CheckButton", "$parentSortButton", GossipFrame, "ChatConfigSmallCheckButtonTemplate")
+SortOption:SetPoint("BOTTOMLEFT",GossipFrame, "BOTTOMLEFT",85,4)
 GossipFrameSortButtonText:SetText("Alphabetical Order")
 SortOption:Hide()
 SortOption:SetScript("OnClick", function(self)
 	if self:GetChecked() then
 		Gshuffle_sort=true
+		SortList()
 	else
 		Gshuffle_sort=false
+		GossipFrame:Update()
 	end
-	GossipFrameUpdate()
 end)
-
-	----Events
-local function eventHandler()
-	if Shown==true then
-		RandomButton:Hide()
-		SortOption:Hide()
-		Shown=false
+------ Sorter function
+local function sorter(a,b)
+	if a.buttonType == 1 then --Gossip title
+	   return true
+	elseif a.buttonType < b.buttonType then --Empty spaces above the songs
+	   return true
+	elseif a.buttonType == b.buttonType and b.buttonType == 3 then --Compare names
+	   return a.info.name < b.info.name
+	elseif a.buttonType == b.buttonType then --hope for the best
+	   return true
 	end
-end
+	return false
+ end
 
-local eventframe = CreateFrame("FRAME")
-eventframe:SetScript("OnEvent", eventHandler)
-eventframe:RegisterEvent("GOSSIP_CLOSED")
-
---------------Click it
-function GossipTitleButton_OnClick(self)--, button)
-	if ( self.type == "Available" ) then
-		C_GossipInfo.SelectAvailableQuest(self:GetID());
-	elseif ( self.type == "Active" ) then
-		C_GossipInfo.SelectActiveQuest(self:GetID());
-	else
-		if Gshuffle_sort==true and Shown==true then
-			C_GossipInfo.SelectOption((Nametable[self:GetText()]));
-		else
-			C_GossipInfo.SelectOption(self:GetID());
-		end
-	end
-end
-
-local function GossipUpdate()
+function SortList()
 	if UnitName("npc")=="Tune-O-Tron 5000" or UnitName("npc")=="B.O.O.M. Box" then
-		Shown=true
 		RandomButton:Show()
 		SortOption:Show()
 		GossipFrameSortButton:SetChecked(Gshuffle_sort)
-		if Gshuffle_sort==true and Shown==true  then
-			GossipFrame.titleButtonPool:ReleaseAll();
-			GossipFrame.buttons = {};
-			--majority of below is from function GossipFrameOptionsUpdate
-			local sortedgossipOptions = {}
-			local unsortedgossipOptions = C_GossipInfo.GetOptions()
-
-			Nametable = {}
-			local sort = {}
-			for i, v in ipairs(unsortedgossipOptions) do
-				Nametable[v.name] = i
-				table.insert(sort,v.name)
-			end
-			table.sort(sort)
-			for i, v in ipairs(sort) do
-				sortedgossipOptions[i] = unsortedgossipOptions[Nametable[v]]
-			end
-
-			for titleIndex, optionInfo in ipairs(sortedgossipOptions) do
-
-				local button = GossipFrame.titleButtonPool:Acquire();
-				table.insert(GossipFrame.buttons, button);
-				button:Show();
-				button:SetOption(optionInfo.name, optionInfo.type);
-
-				button:SetID(titleIndex);
-				--GossipFrame_AnchorTitleButton(button); yay it's a local function code below
-					local buttonCount = GossipFrame_GetTitleButtonCount();
-					if buttonCount > 1 then
-						button:SetPoint("TOPLEFT", GossipFrame_GetTitleButton(buttonCount - 1), "BOTTOMLEFT", 0, (GossipFrame.insertSeparator and -19 or 0) - 3);
-					else
-						button:SetPoint("TOPLEFT", GossipGreetingText, "BOTTOMLEFT", -10, -20);
-					end
-					--GossipFrame_CancelTitleSeparator(); also a local function...
-						GossipFrame.insertSeparator = false;
-					--
-				--
-			end
+		if Gshuffle_sort then
+			GossipFrame.GreetingPanel.ScrollBox:GetDataProvider():SetSortComparator(sorter)
 		end
+	else
+		SortOption:Hide()
+		RandomButton:Hide()
 	end
 end
-hooksecurefunc("GossipFrameUpdate",GossipUpdate)
+
+hooksecurefunc(GossipFrame,"Update", SortList)
+
